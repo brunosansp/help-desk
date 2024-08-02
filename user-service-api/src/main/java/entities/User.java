f0030178054b9958fb44edfd7a5498fdf66745b5
Bruno Santos <brunoadsjp@gmail.com>
2024-08-02 11:23:42 -0300

    feat: add class User

---
+++ user-service-api/src/main/java/entities/User.java
@@ -0,0 +1,26 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+import models.enums.ProfileEnum;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+@Document
+public class User {
+    
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Set<ProfileEnum> profiles;
+}
