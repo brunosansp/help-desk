f824f973098b805cef40f6ddbdea09559ef82bd3
Bruno Santos <brunosansp@outlook.com>
2024-09-27 21:25:45 -0300

    feat: implementing swagger doc

--- user-service-api/src/main/java/br/com/brunosansp/userserviceapi/controller/UserController.java
+++ user-service-api/src/main/java/br/com/brunosansp/userserviceapi/controller/UserController.java
@@ -2,6 +2,14 @@ package br.com.brunosansp.userserviceapi.controller;
 
 import br.com.brunosansp.userserviceapi.service.UserService;
 import entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import models.exceptions.StandardError;
 import models.responses.UserResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "UserController", description = "Controller responsible for user operations")
 @RestController
 @RequestMapping("/api")
 public class UserController {
@@ -27,8 +36,26 @@ public class UserController {
         return userService.findAll();
     }
     
+    @Operation(summary = "Find user by id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "User found"),
+        @ApiResponse(responseCode = "404",
+            description = "User not found",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = StandardError.class))
+        ),
+        @ApiResponse(responseCode = "500",
+            description = "Internal server error",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = StandardError.class))
+        )
+    })
     @GetMapping("user/{id}")
-    public ResponseEntity<UserResponse> byId(@PathVariable String id) {
+    public ResponseEntity<UserResponse> byId(
+        @Parameter(description = "User id", required = true, example = "66b168c5d0cce9437645cbe2")
+        @PathVariable String id) {
         UserResponse user = userService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
