919ec5e9ee7682c899f9533b7b5aaba142e87912
Bruno Santos <brunosansp@outlook.com>
2025-08-07 21:58:35 -0300

    add integration test to find by id user controller with not found

---
+++ user-service-api/src/test/java/br/com/brunosansp/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,55 @@
+package br.com.brunosansp.userserviceapi.controller.impl;
+
+import br.com.brunosansp.userserviceapi.entity.User;
+import br.com.brunosansp.userserviceapi.repository.IUserRepository;
+import models.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static br.com.brunosansp.userserviceapi.create.CreatorUtils.generateMock;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class UserControllerImplTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Test
+    void testFindByIdWithSuccess() throws Exception {
+        final var entity = generateMock(User.class);
+        final var userId = userRepository.save(entity).getId();
+
+        mockMvc.perform(get("/api/users/{id}", userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(entity.getName()))
+                .andExpect(jsonPath("$.email").value(entity.getEmail()))
+                .andExpect(jsonPath("$.password").value(entity.getPassword()))
+                .andExpect(jsonPath("$.profiles").isArray());
+
+        userRepository.deleteById(userId);
+    }
+
+    @Test
+    void testFindByIdWithNotFoundException() throws Exception {
+        mockMvc.perform(get("/api/users/{id}", "987"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Object not found. Id: 987, Type: UserResponse"))
+                .andExpect(jsonPath("$.error").value("ResourceNotFoundException"))
+                .andExpect(jsonPath("$.path").value("/api/users/987"))
+                .andExpect(jsonPath("$.status").value(NOT_FOUND.value()))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
+    }
+}
